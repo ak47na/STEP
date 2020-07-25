00f9f2dc4cff3cce3c0a24f8903290e5c30cd5e7
AnaK <ana.kapros@yahoo.ro>
2020-07-25 09:47:27 +0000

    render login&logout links from LoginStatusServlet using JSON object

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -116,9 +116,11 @@ function updateVisibilityForLoginStatus() {
     if (loginStatus.isLoggedIn === true) {
       // the user is logged in, then unhide commentForm and the logout url
       displayElement('commentForm', true);
+      updateLink('logoutLink', loginStatus.logoutLink);
       displayElement('logoutLink', true);
     } else {
       // unhide login url
+      updateLink('loginLink', loginStatus.loginLink);
       displayElement('loginLink', true);
     }
   });
@@ -134,4 +136,13 @@ function displayElement(elementId, isShown) {
   } else {
     element.style.display = 'none'; 
   }
+}
+
+/**
+ * Display the element with id=elementId if isShown === true, otherwise hide it
+ */
+function updateLink(elementId, link) {
+  element = document.getElementById(elementId);
+
+  element.setAttribute("href", link);
 }
