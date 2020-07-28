57902fd10569c181107d94ebd8b077bb6c72b549
AnaK <ana.kapros@yahoo.ro>
2020-07-28 18:59:14 +0000

    add getUploadedFileUrl() function to return the url to the uploaded file

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -15,12 +15,20 @@
 package com.google.sps.servlets;
 
 import com.google.gson.Gson;
+import com.google.appengine.api.blobstore.BlobInfo;
+import com.google.appengine.api.blobstore.BlobInfoFactory;
+import com.google.appengine.api.blobstore.BlobKey;
+import com.google.appengine.api.blobstore.BlobstoreService;
+import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.Query.SortDirection;
+import com.google.appengine.api.images.ImagesService;
+import com.google.appengine.api.images.ImagesServiceFactory;
+import com.google.appengine.api.images.ServingUrlOptions;
 import com.google.appengine.api.users.User;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
@@ -29,15 +37,22 @@ import com.google.sps.data.Comment;
 // todo: create nickname class that returns the nickname given the id and use it in both 
 // DataServlet and NicknameServlet
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
-import java.sql.Timestamp;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Map;
 import java.util.*; 
+
 import javax.servlet.ServletException;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.nio.charset.StandardCharsets;
+import java.sql.Timestamp;
 
 /** Servlet that handles comments data */
 @WebServlet("/data")
@@ -47,6 +62,7 @@ public class DataServlet extends HttpServlet {
   private DatastoreService datastore;
   private Query commentsQuery;
   private UserService userService; 
+
 
   /** Initializes data needed to load comments from datastore when requested */
   @Override
@@ -81,7 +97,7 @@ public class DataServlet extends HttpServlet {
   }
 
   @Override
-  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     // Get the comment from the form
     String newComment = null;
     try {
@@ -98,13 +114,66 @@ public class DataServlet extends HttpServlet {
     String userEmail = currentUser.getEmail();
     String userId = currentUser.getUserId();
     datastore.put(createCommentEntity(newComment, userEmail, userId));
+    String imageUrl = getUploadedFileUrl(request, "image");
+
+    response.setContentType("text/html");
+    PrintWriter out = response.getWriter();
+    out.println("<p>Here's the image you uploaded:</p>");
+    out.println("<a href=\"" + imageUrl + "\">");
+    out.println("<img src=\"" + imageUrl + "\" />");
+    out.println("</a>");
+    System.out.println(imageUrl);
 
     // Redirect back to the HTML page.
-    response.sendRedirect("/index.html");
+    RequestDispatcher requestDispatcher = request.getRequestDispatcher("index/html");
+    requestDispatcher.include(request, response);
+  }
+
+  /** Returns a URL that points to the uploaded file, or null if the user didn't upload a file. */
+  private String getUploadedFileUrl(HttpServletRequest request, String formInputElementName) {
+    BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
+    // getUploads returns the BlobKey for any files that were uploaded, keyed by the "name" field 
+    // of the upload form.
+    Map<String, List<BlobKey>> blobs = blobstoreService.getUploads(request);
+    List<BlobKey> blobKeys = blobs.get("image");
+
+    // User submitted form without selecting a file, so we can't get a URL. (dev server)
+    if (blobKeys == null || blobKeys.isEmpty()) {
+      return null;
+    }
+
+    // commentForm only contains a single file input, so get the first index.
+    BlobKey blobKey = blobKeys.get(0);
+
+    // User submitted form without selecting a file, so we can't get a URL. (live server)
+    BlobInfo blobInfo = new BlobInfoFactory().loadBlobInfo(blobKey);
+    if (blobInfo.getSize() == 0) {
+      blobstoreService.delete(blobKey);
+      return null;
+    }
+
+    // check that the user submitted an image file
+    if (!blobInfo.getContentType().contains("image/")) {
+      return null;
+    }
+
+    // Use ImagesService to get a URL that points to the uploaded file.
+    ImagesService imagesService = ImagesServiceFactory.getImagesService();
+    ServingUrlOptions options = ServingUrlOptions.Builder.withBlobKey(blobKey);
+
+    // To support running in Google Cloud Shell with AppEngine's devserver, we must use the relative
+    // path to the image, rather than the path returned by imagesService which contains a host.
+    try {
+      //obtains a URL that can dynamically serve the image stored as a blob.
+      URL url = new URL(imagesService.getServingUrl(options));
+      return url.getPath();
+    } catch (MalformedURLException e) {
+      return imagesService.getServingUrl(options);
+    }
   }
 
   /** Returns an array with at most limit Comment objects */
-  private List<Comment> getCommentsArray(int limit) {
+  private List<Comment> getCommentsArray(int limit) throws IOException{
     PreparedQuery results = datastore.prepare(commentsQuery);
 
     List<Comment> comments = new ArrayList<>();
