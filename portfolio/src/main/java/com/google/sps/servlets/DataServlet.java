667fe71f37f608d264ae86471b5d5f9e8d0a2d85
AnaK <ana.kapros@yahoo.ro>
2020-07-28 12:00:33 +0000

    fix DataServlet bugs introduced in merge conflicts and add changeNicknameLink to index.html

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -14,6 +14,7 @@
 
 package com.google.sps.servlets;
 
+
 import com.google.gson.Gson;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
@@ -25,6 +26,8 @@ import com.google.appengine.api.users.User;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
 import com.google.sps.data.Comment;
+
+// todo: create nickname class that return nickname given the id and use it in both servlets
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.sql.Timestamp;
@@ -39,12 +42,16 @@ import java.nio.charset.StandardCharsets;
 /** Servlet that handles comments data */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
+  
   public static final int MAX_COMMENTS = 100;
   private DatastoreService datastore;
   private Query commentsQuery;
+  private UserService userService; 
+
   /** Initializes data needed to load comments from datastore when requested */
   @Override
   public void init() {
+    userService = UserServiceFactory.getUserService();
     datastore = DatastoreServiceFactory.getDatastoreService();
     commentsQuery = new Query("Comment").addSort("timestamp", SortDirection.DESCENDING);
   }
@@ -87,9 +94,6 @@ public class DataServlet extends HttpServlet {
       response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
     }
 
-    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-
-    UserService userService = UserServiceFactory.getUserService();
     User currentUser = userService.getCurrentUser();
     String userEmail = currentUser.getEmail();
     String userId = currentUser.getUserId();
@@ -98,25 +102,8 @@ public class DataServlet extends HttpServlet {
     // Redirect back to the HTML page.
     response.sendRedirect("/index.html");
   }
-  /*
-    Get the maximum number of comments to be displayed from the queryString
-  */
-  private String getCommentsLimit(HttpServletRequest request) {
-    if (request.getQueryString() == null) {
-      return "5";
-    }
-    String[] queryStringArray = request.getQueryString().split("&");
 
-    for (String keyValuePair : queryStringArray) {
-      String[] keyValuePairArray = keyValuePair.split("=");
-      if (keyValuePairArray[0].equals("commentsLimit")) {
-        return keyValuePairArray[1];
-      }
-    }
-    // return default value if none was provided
-    return "5";
-  }
-
+  /** Returns an array with at most limit Comment objects */
   private List<Comment> getCommentsArray(int limit) {
     PreparedQuery results = datastore.prepare(commentsQuery);
 
@@ -127,9 +114,7 @@ public class DataServlet extends HttpServlet {
       }
       -- limit;
       String nickname = (String)entity.getProperty("userNickname");
-      
       if (nickname == null) {
-        // if the user has no nickname, use the email address instead 
         nickname = (String)entity.getProperty("userEmail");
       }
       Comment comment = new Comment((String)entity.getProperty("message"), nickname);
@@ -157,16 +142,18 @@ public class DataServlet extends HttpServlet {
     byte[] commentBytes = newComment.getBytes("UTF-8");
     return newComment;
   }
-  
+
+  /** Returns the nickname of the user or null if no nickname was set by the user */
   private String getUserNickname(String userId) {
     Query query = new Query("UserInfo").setFilter(new Query.FilterPredicate("id", Query.FilterOperator.EQUAL, userId));
     PreparedQuery result = datastore.prepare(query);
-
+    
     Entity entity = result.asSingleEntity();
     String nickname = null;
     if (entity != null) {
       nickname = (String) entity.getProperty("nickname");
     }
+    System.out.println("nickname");
 
     return nickname;
   }
